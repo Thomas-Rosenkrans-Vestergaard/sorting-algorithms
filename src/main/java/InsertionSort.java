bf2094dcd383fb4d5cacd478b34af93e14b166c0
Thomas <Thomas-Rosenkrans-Vestergaard@users.noreply.github.com>
2017-11-29 15:22:34 +0100

    InsertionSort documentation

--- src/main/java/InsertionSort.java
+++ src/main/java/InsertionSort.java
@@ -2,6 +2,14 @@ import java.util.Comparator;
 
 public class InsertionSort implements SortingAlgorithm
 {
+
+	/**
+	 * Implementation of insertion-sort algorithm using a provided comparator.
+	 *
+	 * @param array      The array.
+	 * @param comparator The comparator.
+	 * @param <T>        The type of the elements in the provided array.
+	 */
 	@Override public <T> void sort(T[] array, Comparator<T> comparator)
 	{
 		for (int i = 1; i < array.length; i++) {
